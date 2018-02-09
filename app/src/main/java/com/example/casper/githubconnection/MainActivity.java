9df458f9416db638608e608a519d1080d49a7ac8
CASPER <flori-dule@hotmail.com>
2018-02-09 14:34:42 +0300

    ilk

--- app/src/main/java/com/example/casper/githubconnection/MainActivity.java
+++ app/src/main/java/com/example/casper/githubconnection/MainActivity.java
@@ -10,7 +10,5 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toast.makeText(this, "Toast message", Toast.LENGTH_SHORT).show();
-        Toast.makeText(this, "Toast message", Toast.LENGTH_SHORT).show();
     }
 }
